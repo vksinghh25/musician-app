77c9696bba8a84d27377a63bcf3202128dc69dbd
vksinghh25 <vipkumar@expedia.com>
2020-07-18 22:22:09 +0530

    change status method

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: I'm good!");
 });
 
 // retrieve all musicians from data store
